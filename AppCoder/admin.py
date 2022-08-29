b962398561d057c64bf91b099a04f7f1f58c1311
Pedro Rojas Gavidia <pedrorojas89@gmail.com>
2022-08-29 19:47:00 -0400

    Agregado admin al AppCoder

---
+++ AppCoder/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from AppCoder.models import Estudiante, Entregable, Profesor, Curso
+
+admin.site.register(Estudiante)
+admin.site.register(Entregable)
+admin.site.register(Profesor)
+admin.site.register(Curso)
