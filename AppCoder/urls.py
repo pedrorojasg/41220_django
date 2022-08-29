0a8bcd9cda966dd2c23c210bd0cd3c91e02a6bc3
Pedro Rojas Gavidia <pedrorojas89@gmail.com>
2022-08-29 19:34:55 -0400

    Agregada navegación a la pagina

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,9 +4,9 @@ from AppCoder import views
 
 urlpatterns = [
     path('', views.inicio), #esta era nuestra primer view
-    path('cursos/', views.cursos, name="Cursos"),
-    path('profesores/', views.profesores),
-    path('estudiantes/', views.estudiantes),
+    path('cursos/', views.cursos, name="cursos"),
+    path('profesores/', views.profesores, name="profesores"),
+    path('estudiantes/', views.estudiantes, name="estudiantes"),
     path('entregables/', views.entregables),
 ]
 
