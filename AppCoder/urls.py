8b863f9436b7b40ea3e394ca4dfdc25dcb43c493
Pedro Rojas Gavidia <pedrorojas89@gmail.com>
2022-08-31 18:47:31 -0400

    Agregada navegacion al formulario de crear curso

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('cursos/', views.cursos, name="cursos"),
     path('profesores/', views.profesores, name="profesores"),
     path('estudiantes/', views.estudiantes, name="estudiantes"),
-    path('entregables/', views.entregables),
+    path('entregables/', views.entregables, name="entregables"),
     path('crear-curso/', views.curso_formulario, name="curso_formulario"),
 ]
