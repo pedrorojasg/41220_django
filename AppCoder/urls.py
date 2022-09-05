a3afec39e026525790c16a7de48368e2c9410160
Pedro Rojas Gavidia <pedrorojas89@gmail.com>
2022-09-05 16:49:11 -0400

    Mejor organización URLs de la app

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,11 +4,13 @@ from AppCoder import views
 
 urlpatterns = [
     path('', views.inicio, name="inicio"),
-    path('cursos/', views.cursos, name="cursos"),
-    # path('profesores/', views.profesores, name="profesores"),
     path('estudiantes/', views.estudiantes, name="estudiantes"),
     path('entregables/', views.entregables, name="entregables"),
+    # URLs de Cursos
+    path('cursos/', views.cursos, name="cursos"),
     path('crear-curso/', views.curso_formulario, name="curso_formulario"),
     path('busqueda-curso-form/', views.busqueda_cursos, name="busqueda_curso_form"),
     path('busqueda-curso/', views.buscar_curso, name="busqueda_curso"),
+    # URLs de Profesores
+    path('profesores/', views.profesores, name="profesores"),
 ]
