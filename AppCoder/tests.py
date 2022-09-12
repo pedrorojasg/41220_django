3754ca5bc08982af2045e454cd29600cfb53798e
Pedro Rojas Gavidia <pedrorojas89@gmail.com>
2022-09-12 19:58:16 -0400

    Agregado tests unitarios al modelo Profesor

---
+++ AppCoder/tests.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+from django.test import TestCase
+from AppCoder.models import Profesor
+
+class ProfesorTestCase(TestCase):
+    pass
+
+    def test_creacion_profesores(self):
+        # Test 1: Comprobar puedo crear un profesor con un nombre con letras random
+        lista_letras_nombre = [random.choice(string.ascii_letters + string.digits) for _ in range(20)]
+        lista_letras_apellido = [random.choice(string.ascii_letters + string.digits) for _ in range(20)]
+        nombre_prueba = "".join(lista_letras_nombre)
+        apellido_prueba = "".join(lista_letras_apellido)
+        profesor_1 = Profesor.objects.create(nombre=nombre_prueba, apellido=apellido_prueba)
+
+        self.assertIsNotNone(profesor_1.id)
+        self.assertEqual(profesor_1.nombre, nombre_prueba)
+        self.assertEqual(profesor_1.apellido, apellido_prueba)
