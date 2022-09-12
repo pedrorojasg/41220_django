f209de91e3c3350672820da788795503660951b7
Pedro Rojas Gavidia <pedrorojas89@gmail.com>
2022-09-12 17:24:47 -0400

    Agregado manejo de imagenes

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from AppCoder.models import Estudiante, Entregable, Profesor, Curso
+from AppCoder.models import Estudiante, Entregable, Profesor, Curso, Avatar
 
 admin.site.register(Estudiante)
 admin.site.register(Entregable)
 admin.site.register(Profesor)
 admin.site.register(Curso)
+admin.site.register(Avatar)
